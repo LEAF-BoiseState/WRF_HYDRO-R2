25cc1417c7bed30ae62e678e63f7a6e5d80ef110
Matt Masarik <mattmasarik@boisestate.edu>
2019-07-07 23:59:48 -0600

    create_hydro_namelist.sh - all 5 routing sections implemented. Still needs polishing and testing.

--- run_scripts/create_hydro_namelist.sh
+++ run_scripts/create_hydro_namelist.sh
@@ -13,49 +13,87 @@
 
 
 
+
+# INPUT
+routing_options=""
+routing_options_flag="false"
+num_routing_options=0
+if [ "$#" -gt 5 ]; then
+    prog=$(basename $0)
+    echo -e "\n\tUSAGE: $prog [<opt1> ... <opt5>]   # <optn> = n\n"
+    exit 1
+elif [ "$#" -ge 1 ]; then
+    routing_options="$*"
+    num_routing_options=$#
+    routing_options_flag="true"
+fi
+
+
+SUB_FLAG=0                   # 1 SUB: Subsurface
+OVR_FLAG=0                   # 2 OVR: Overland
+CHL_FLAG=0                   # 3 CHL: Channel
+RES_FLAG=0                   # 4 RES: Lakes / Reservoirs
+GWB_FLAG=0                   # 5 GWB: Groundwater / Base flow
+for ro in $routing_options
+do
+    if [ $ro -eq 1 ]; then SUB_FLAG=1; fi
+    if [ $ro -eq 2 ]; then OVR_FLAG=1; fi
+    if [ $ro -eq 3 ]; then CHL_FLAG=1; fi
+    if [ $ro -eq 4 ]; then RES_FLAG=1; fi
+    if [ $ro -eq 5 ]; then GWB_FLAG=1; fi
+done
+
+
 # ----------------------------------------------------------------------------- *
 #                      GENERAL SIMULATION PARAMETERS
 # ----------------------------------------------------------------------------- *
 # Specify the restart file write frequency...(minutes)
 # A value of -99999 will output restarts on the first day of the month only.
-rst_dt=9999       
-
-# Reset the LSM soil states from the high-res routing restart file 
-# (1=overwrite, 0=no overwrite)
-# NOTE: Only turn this option on if overland or subsurface rotuing is active!
-rst_typ=0
-
-# Netcdf grid of variables passed between LSM and routing components (2d)
-# (0 = no output, 1 = output)
-# NOTE: No scale_factor/add_offset available
-LSMOUT_DOMAIN=1
-
-# Netcdf grid of terrain routing variables on routing grid (2d):
-# (0 = no output, 1 = output)
-RTOUT_DOMAIN=1
-
-# ASCII text file of forecast points or gage points (defined in Routelink):
-# (0 = no output, 1 = output)
-frxst_pts_out=1
-
-# Specify the grid spacing of the terrain routing grid...(meters)
-DXRT=250.0
-
-# Integer multiple between land model grid and terrain routing grid...(integer)
-AGGFACTRT=4
-
-# Specify the terrain routing model timestep...(seconds)
-DTRT_TER=10
-
-
-
-
+rst_dt=-99999       
 
 
 
 # ----------------------------------------------------------------------------- *
 #                      (0) LSM - NOAHMP LAND SURFACE
 # ----------------------------------------------------------------------------- *
+# Netcdf grid of variables passed between LSM and routing components (2d)
+# (0 = no output, 1 = output)
+# NOTE: No scale_factor/add_offset available
+LSMOUT_DOMAIN=1
+
+
+
+# ----------------------------------------------------------------------------- *
+#                      (0.5) TERRAIN - SUBSURFACE / OVERLAND
+# ----------------------------------------------------------------------------- *
+lsm_rst_type=0                            
+terrain_routing_grid_output=0
+if [[ $SUB_FLAG -eq 1 ]] || [[ $OVR_FLAG -eq 1 ]]; then
+    lsm_rst_type=1
+    terrain_routing_grid_output=1
+fi
+
+# Reset the LSM soil states from the high-res routing restart file 
+# (1=overwrite, 0=no overwrite)
+# NOTE: Only turn this option on if overland or subsurface rotuing is active!
+rst_typ=$lsm_rst_type
+
+# Netcdf grid of terrain routing variables on routing grid (2d):
+# (0 = no output, 1 = output)
+RTOUT_DOMAIN=$terrain_routing_grid_output
+
+# Specify the terrain routing model timestep...(seconds)
+DTRT_TER=10
+
+# Specify the grid spacing of the terrain routing grid...(meters)
+DXRT=250
+
+# Created parameter, grid spacing of the LSM grid (meters), file: geo_em.d0x.nc
+# NOTE: this is along with DXRT to calculate AGGFACTRT
+DXLSM=1000 
+
+# Integer multiple between land model grid and terrain routing grid...(integer)
+AGGFACTRT=$(( DXLSM / DXRT ))
 
 
 
@@ -64,9 +102,14 @@ DTRT_TER=10
 # ----------------------------------------------------------------------------- *
 #                      (1) SUB - SUBSURFACE
 # ----------------------------------------------------------------------------- *
+subrtswcrt_val=0
+if [ $SUB_FLAG -eq 1 ]; then
+    subrtswcrt_val=1
+fi
+
 # Switch to activate subsurface routing:
 # (0=no, 1=yes)
-SUBRTSWCRT=0
+SUBRTSWCRT=$subrtswcrt_val
 
 
 
@@ -74,9 +117,14 @@ SUBRTSWCRT=0
 # ----------------------------------------------------------------------------- *
 #                      (2) OVR - OVERLAND
 # ----------------------------------------------------------------------------- *
+ovrtswcrt_val=0
+if [ $OVR_FLAG -eq 1 ]; then
+    ovrtswcrt_val=1
+fi
+
 # Switch to activate surface overland flow routing:
 # (0=no, 1=yes)
-OVRTSWCRT=1
+OVRTSWCRT=$ovrtswcrt_val
 
 # Specify overland flow routing option:
 # (1=Steepest Descent - D8, 2=CASC2D - not active)
@@ -89,9 +137,21 @@ rt_option=1
 # ----------------------------------------------------------------------------- *
 #                      (3) CHL - CHANNEL
 # ----------------------------------------------------------------------------- *
+chanrtswcrt_val=0
+chrtout_domain_val=0
+chanobs_domain_val=0
+frxst_pts_out_val=0
+if [ $CHL_FLAG -eq 1 ]; then
+    chanrtswcrt_val=1
+    chrtout_domain_val=1
+    chanobs_domain_val=1
+    frxst_pts_out_val=1
+fi
+
+
 # Switch to activate channel routing:
 # (0=no, 1=yes)
-CHANRTSWCRT=1
+CHANRTSWCRT=$chanrtswcrt_val
 
 # Specify channel routing option:
 # (1=Muskingam-reach, 2=Musk.-Cunge-reach, 3=Diff.Wave-gridded)
@@ -102,16 +162,24 @@ DTRT_CH=10
 
 # Netcdf point timeseries output at all channel points (1d):
 # (0 = no output, 1 = output)
-CHRTOUT_DOMAIN=1
+CHRTOUT_DOMAIN=$chrtout_domain_val
 
 # Netcdf point timeseries at forecast points or gage points (defined in Routelink):
 # (0 = no output, 1 = output at forecast points or gage points.)
-CHANOBS_DOMAIN=1
+CHANOBS_DOMAIN=$chanobs_domain_val
 
 # Netcdf grid of channel streamflow values (2d):
 # (0 = no output, 1 = output)
 # NOTE: Not available with reach-based routing
-CHRTOUT_GRID=1
+chrtout_grid_val=0
+if [[ $CHL_FLAG -eq 1 ]] && [[ $channel_option -eq 3 ]]; then
+    chrtout_grid_val=1
+fi
+CHRTOUT_GRID=$chrtout_grid_val
+
+# ASCII text file of forecast points or gage points (defined in Routelink):
+# (0 = no output, 1 = output)
+frxst_pts_out=$frxst_pts_out_val
 
 
 
@@ -119,9 +187,14 @@ CHRTOUT_GRID=1
 # ----------------------------------------------------------------------------- *
 #                      (4) RES - LAKE / RESERVOIR
 # ----------------------------------------------------------------------------- *
+outlake_val=0
+if [ $RES_FLAG -eq 1 ]; then
+    outlake_val=1
+fi
+
 # Netcdf grid of lake values (1d):
 # (0 = no output, 1 = output)
-outlake=0
+outlake=$outlake_val
 
 
 
@@ -129,17 +202,25 @@ outlake=0
 # ----------------------------------------------------------------------------- *
 #                      (5) GWB - GROUNDWATER / BASEFLOW
 # ----------------------------------------------------------------------------- *
+gwbaseswcrt_val=0
+output_gw_val=0
+if [ $GWB_FLAG -eq 1 ]; then
+    gwbaseswcrt_val=1               # [1, 2] default=1 (exp. bucket)
+    output_gw_val=1
+fi
+gw_restart_val=0                    # [0, 1] default=0 (cold start from tbl)
+
 # Switch to activate baseflow bucket model:
 # (0=none, 1=exp. bucket, 2=pass-through)
-GWBASESWCRT=0
+GWBASESWCRT=$gwbaseswcrt_val        # default=1, exp bucket, when gwb on
 
 # Specify baseflow/bucket model initialization:
 # (0=cold start from table, 1=restart file)
-GW_RESTART=0
+GW_RESTART=$gw_restart_val          # default=0, cold start from tbl, when gwb on
 
 # Netcdf GW output:
 # (0 = no output, 1 = output)
-output_gw=0
+output_gw=$output_gw_val
 
 
 
